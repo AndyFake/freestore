123deba93de83e80c69f32e51a7d590d65e313da
Andy March <andymarch@A-Marchs-MBP.hitronhub.home>
2018-07-23 10:24:40 -0700

    add_meta_data

--- parseStore.js
+++ parseStore.js
@@ -1,4 +1,38 @@
 const fs = require('fs')
 const data = fs.readFileSync('./store.txt')
-const output = `const data =  \`` + data + `\`` + "\nmodule.exports={data}"
+
+const lines = String(data)
+.split('\n')
+.filter(Boolean)
+.map((line,i)=>({lineNumber:i+1,text:line}))
+.map((line,i)=>i<5
+  ?({lineNumber:line.lineNumber,text:line.text.split(':')[1]})
+  :line)
+
+const [name,banner,logo,favicon,currency,email,...productList] = lines
+
+toGroupsOf= x=> data=> data.length>=x
+  ? [data.slice(0,x),...toGroupsOf(x)(data.slice(x))]
+  : []
+
+const products = toGroupsOf(4)(productList).map(x=>(
+  {name:x[0],
+  price:x[1],
+  description:x[2],
+  image:x[3]
+  }
+))
+
+const output = `const data =  \`` + 
+  JSON.stringify({
+    name,
+    banner,
+    logo,
+    favicon,
+    currency,
+    products,
+  }) + 
+  `\`` + 
+  "\nmodule.exports={data}"
+
 fs.writeFileSync('./src/store.js',output)
--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {data} from './store.js'
+import {data} from './store.js';
+const {name,banner,logo,favicon,currency,products} = JSON.parse(data)
 
-const imageList = data.split('\n')
-.filter(x=>x!='')
-.map(line=>{
-  const directories = line.split('/')
-  return directories[directories.length-1]
-})
-
-const images = imageList.map(imageName=>{
-  const imageUrl = '/images/'+imageName
-  return(
-    <img src={imageUrl}/>
-  )
-})
+class Product extends Component {
+  render(){
+    const {name,price,description,image} = this.props.data
+    return(
+      <div className="Product">
+        <img src={'/images/'+image.text}/>
+        <div className="Product-bar">
+          <div className="Product-name">{name.text}</div>
+          <div className="Product-price">{price.text}</div>
+        </div>
+        <div className="Product-description">{description.text}</div>
+        <div className="Add-to-cart">add to cart</div>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        {imageList.map(x=><p>{x}</p>)}
-        </p>
-        {images}
+        <img src={`/images/${banner.text}`}/>
+        <div className='Store-title'>{name.text}</div>
+        {products.map(product=><Product data={product}/>)}
       </div>
     );
   }
